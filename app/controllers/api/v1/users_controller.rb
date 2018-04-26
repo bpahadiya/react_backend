5b3f63432068296b1694825bdb6dc3dbaecf0f46
Bhawna <bpahadiya@gmail.com>
2018-04-26 18:34:52 +0530

    api for sign_up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,17 @@
 class Api::V1::UsersController < ApplicationController
 
   def signup
-    user = User.new(email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
 
+    user = User.new user_params
     if user.save
       render json: {success: true, message: "user created"}, status: 201
     else
-      render json: {success: false, errors: user.errors}, status: 422
+      render json: {success: false, errors: user.errors.full_messages}, status: 422
     end
   end
+
+  protected
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :dob, :contact_number, :role)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   validates :role, :last_name, :first_name, presence: true
 end
